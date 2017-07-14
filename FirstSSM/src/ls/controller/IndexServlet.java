881f9d5aa882616fecf8120d4c1160e31168d6bb
fendou666 <ls_code@126.com>
2017-07-14 19:27:51 +0800

    第一版SSM框架整合

---
+++ FirstSSM/src/ls/controller/IndexServlet.java
@@ -0,0 +1,35 @@
+package ls.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ls.model.Student;
+import ls.service.IStudentService;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class IndexServlet extends AbstractController {
+	private IStudentService stuService;
+	
+	public IStudentService getStuService() {
+		return stuService;
+	}
+
+	public void setStuService(IStudentService stuService) {
+		System.out.println("set stuService");
+		this.stuService = stuService;
+	}
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		Student stu = stuService.selectByPrimaryKey(3L);
+		ModelAndView modelAndView = new ModelAndView();
+		System.out.println("进入 index 请求");
+		modelAndView.addObject("userName", stu.getName());
+		modelAndView.setViewName("/WEB-INF/jsp/login.jsp");
+		return modelAndView;
+	}
+
+}
---
+++ FirstSSM/src/ls/service/StudentServiceImp.java
@@ -0,0 +1,47 @@
+package ls.service;
+
+import ls.dao.StudentMapper;
+import ls.model.Student;
+
+public class StudentServiceImp implements IStudentService {
+	private StudentMapper stuMap;
+	
+	public StudentMapper getStuMap() {
+		return stuMap;
+	}
+
+	public void setStuMap(StudentMapper stuMap) {
+		this.stuMap = stuMap;
+	}
+
+	@Override
+	public int deleteByPrimaryKey(Long id) {
+		return stuMap.deleteByPrimaryKey(id);
+	}
+
+	@Override
+	public int insert(Student record) {
+		return stuMap.insert(record);
+	}
+
+	@Override
+	public int insertSelective(Student record) {
+		return stuMap.insertSelective(record);
+	}
+
+	@Override
+	public Student selectByPrimaryKey(Long id) {
+		return stuMap.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public int updateByPrimaryKeySelective(Student record) {
+		return stuMap.updateByPrimaryKeySelective(record);
+	}
+
+	@Override
+	public int updateByPrimaryKey(Student record) {
+		return stuMap.updateByPrimaryKey(record);
+	}
+
+}
